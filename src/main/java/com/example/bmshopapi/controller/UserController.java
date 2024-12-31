11b044d6530c5c302bf8535462be675e76c3e08b
PhamHoang16 <hoangpham1618@gmail.com>
2024-12-31 19:48:17 +0700

    update cors user controller

--- src/main/java/com/example/bmshopapi/controller/UserController.java
+++ src/main/java/com/example/bmshopapi/controller/UserController.java
@@ -1,18 +1,22 @@
 package com.example.bmshopapi.controller;
 
+import com.example.bmshopapi.entity.User;
+import com.example.bmshopapi.repository.UserRepository;
 import com.example.bmshopapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @GetMapping("{userId}")
-    public ResponseEntity<?> getUser(@RequestParam String userId) {
+    public ResponseEntity<?> getUser(@PathVariable String userId) {
         return ResponseEntity.ok(userService.getUser(userId));
     }
 
@@ -20,5 +24,10 @@ public class UserController {
     public ResponseEntity<?> deposit(@RequestParam String userId, @RequestParam double amount) {
         userService.deposit(userId, amount);
         return ResponseEntity.ok("Deposit successful");
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userRepository.save(user));
     }
 }
